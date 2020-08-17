0e322d5043ac965a55add71ce8ca4257319429df
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-08-17 18:04:42 -0300

    Update admin.py

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, Profile, Posts
 
 # Register your models here.
+
+class Admin(admin.ModelAdmin):
+    filter_horizontal = ("network",)
+
+
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Posts)
