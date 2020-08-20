4cf42226544cc0416c4caf7e0f9d60b2cf2237cd
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-08-20 12:30:17 -0300

    Update urls.py

--- network/urls.py
+++ network/urls.py
@@ -2,9 +2,11 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 from . import views
 from .views import index, login_view, logout_view, register, profile, post_view
 
+app_name = 'network'
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
