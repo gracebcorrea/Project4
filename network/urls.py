644d118f666c8c367166742bdc4b47f52e388b38
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-08-19 21:31:47 -0300

    Update urls.py

--- network/urls.py
+++ network/urls.py
@@ -1,7 +1,9 @@
-
-from django.urls import path
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
+from .views import index, login_view, logout_view, register, profile, post_view
 
 urlpatterns = [
     path("", views.index, name="index"),
@@ -10,6 +12,7 @@ urlpatterns = [
     path("register", views.register, name="register"),
 
 
-    path("profile",views.profile, name="profile")
+    path("profile",views.profile, name="profile"),
+    path("post",views.post_view, name="post"),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
