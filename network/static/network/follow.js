e2e0acfe9b7fc7c8cabd246e8e439730d8078f64
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-08-22 14:38:51 -0300

    Update follow.js

--- network/static/network/follow.js
+++ network/static/network/follow.js
@@ -5,15 +5,17 @@ document.addEventListener('DOMContentLoaded', function () {
 function followme() {
   const url = "/followme";
   console.log("Follow me:");
-  profile_id = document.querySelector('#profile_id').value;
-  profile_user = document.querySelector('#profile_user').value;
+//  profile_id = document.querySelector('#profile_id').value;
+//  profile_user = document.querySelector('#profile_user').value;
+profile_id = document.getElementById('profile_id').value;
+profile_user = document.getElementById('profile_user').value;
 
-  console.log(`${profile_id}`,`${profile_user}`);
+  console.log("Values are:", `${profile_id}`,`${profile_user}`);
 
   fetch(url, {
            method: "PUT",
            body: JSON.stringify({
-           profileid: `${profile_id}`,
+           id: `${profile_id}`,
            follower: `${user}`,
         })
   })
