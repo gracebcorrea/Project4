f75a8dfb8b6b16cf49bbf4c92878058979c695d9
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-08-22 02:17:43 -0300

    Update follow.js

--- network/static/network/follow.js
+++ network/static/network/follow.js
@@ -1,11 +1,15 @@
-document.querySelector('#followme').addEventListener('click', () => followme());
+document.addEventListener('DOMContentLoaded', function () {
+
+  document.querySelector('#followme').addEventListener('click', () => followme());
+
+}
 
 function followme(){
   const url = "followme";
   console.log("Follow me:");
   profile_id = document.querySelector('#profile_id').value;
   profile_user = document.querySelector('#profile_user').value;
-
+  alert(profile_id,profile_user);
   console.log(`${profile_id}`,`${profile_user}`);
   fetch(url, {
       method: "PUT",
@@ -23,3 +27,4 @@ function following(){
 
 
 }
+//chrome.exe --user-data-dir="D:\CS50"
