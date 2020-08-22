872018f6be480fd19d11113b1de9ffb5bfd1b13a
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-08-22 00:51:47 -0300

    Update followscripts.js

--- network/static/network/followscripts.js
+++ network/static/network/followscripts.js
@@ -1,13 +1,16 @@
-  function followme(){
 
-
-
-
-
-
-
-
-  }
+function followme(profile_id,user){
+  const url = "followme";
+  console.log("Followme:");
+  console.log(`${profile_id}`,`${user}`);
+  fetch(url, {
+      method: "PUT",
+      body: JSON.stringify({
+      profileid: `${profile_id}`,
+      follower: `${user}`,
+      }),
+  });
+}
 
 function following(){
 
