496ef2468494f9e2987f23218554811b70ada04d
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-08-22 13:15:15 -0300

    Update follow.js

--- network/static/network/follow.js
+++ network/static/network/follow.js
@@ -1,30 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
-
   document.querySelector('#followme').addEventListener('click', () => followme());
-
-}
+});
 
 function followme(){
   const url = "followme";
   console.log("Follow me:");
   profile_id = document.querySelector('#profile_id').value;
   profile_user = document.querySelector('#profile_user').value;
-  alert(profile_id,profile_user);
+
   console.log(`${profile_id}`,`${profile_user}`);
+
   fetch(url, {
-      method: "PUT",
-      body: JSON.stringify({
-      profileid: `${profile_id}`,
-      follower: `${user}`,
-      }),
-  });
+           method: "PUT",
+           body: JSON.stringify({
+           profileid: `${profile_id}`,
+           follower: `${user}`,
+        })
+  })
+  .then((response) => response.json())
+  .then((res) => {
+      if (res.status == 201) {
+        console.log("adicionar novo seguidor no total")
+      }
+  })
 }
+
 
 function following(){
-
-
-
-
-
 }
+
+
+
+
 //chrome.exe --user-data-dir="D:\CS50"
