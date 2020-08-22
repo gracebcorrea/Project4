845012171c2240e3bc4ad29a62072f0b268365aa
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-08-22 16:42:25 -0300

    Update follow.js

--- network/static/network/follow.js
+++ network/static/network/follow.js
@@ -3,27 +3,29 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function followme() {
+
   const url = "/followme";
   console.log("Follow me:");
-//  profile_id = document.querySelector('#profile_id').value;
-//  profile_user = document.querySelector('#profile_user').value;
-profile_id = document.getElementById('profile_id').value;
-profile_user = document.getElementById('profile_user').value;
-profile_ForN = document.getElementById('follow_me').value;
+  //profile id in view
+  profile_id = document.getElementById('profile_id').value;
+  //profile name in view
+  profile_Searcher = document.getElementById('profile_Searcher').value;
+  //action
+  profile_ForN = document.getElementById('follow_me').value;
 
   console.log("Values are:", `${profile_id}`,`${profile_user}`, `${profile_ForN}`);
 
   fetch(url, {
-           method: "PUT",
-           body: JSON.stringify({
-           id: `${profile_id}`,
-           follower: `${profile_user}`,
-           fornot: `${profile_ForN}`,
-        })
+          method: "PUT",
+          body: JSON.stringify({
+                id: `${profile_id}`,
+                follower: `${profile_Searcher}`,
+                fornot: `${profile_ForN}`,
+          })
   })
   .then((response) => response.json())
-  .then((res) => {
-      if (res.status == 201) {
+  .then((response) => {
+      if (response.status == 201) {
         console.log("adicionar novo seguidor no total")
       }
   })
