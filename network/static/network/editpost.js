597667d11ce15688099f497c7e02fb8acbc2704a
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-08-25 16:57:28 -0300

    Update editpost.js

--- network/static/network/editpost.js
+++ network/static/network/editpost.js
@@ -4,14 +4,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
         console.log('EditPost clicked!');
         //Get data from form
-        postid = document.getElementById('post_id').value;
-        page = document.getElementById('pagename').value;
-        oldpost = document.getElementById('oldtext').value;
-        console.log(page,postid)
+        postid = document.querySelector('#post_id').value;
+        page = document.querySelector('#pagename').value;
+        oldpost = document.querySelector('#oldtext').value;
+
         document.querySelector('#newtext').innerHTML = `
                 <form>
-                    <textarea class="form-control">oldpost</textarea>
-                    <input id="submit" type="submit" class="btn btn-primary"/>
+                    <textarea class="form-control" initial=${oldpost} ></textarea>
+                    <input id="submitchange" type="submit" class="btn btn-primary"/>
 
                 </form>
 
