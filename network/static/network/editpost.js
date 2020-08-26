2656e288654fca453de47eb9f8c32b25203b1f4b
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-08-26 17:12:58 -0300

    Update editpost.js

--- network/static/network/editpost.js
+++ network/static/network/editpost.js
@@ -3,37 +3,17 @@ document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('#newtext').style.display = 'none';
 });
 
-function query_post(postid, page, oldpost){
-  const spostid = postid;
-  const spage = page;
-  const soldpost = oldpost;
-  alert('EditPost clicked!');
-  document.querySelector('#oldtext').style.display = 'none';
-  document.querySelector('#newtext').style.display = 'block';
 
-  //Get data from form
-
-  console.log(`${spostid}`, `${spage}`, `${soldpost}`);
-
-  document.querySelector(`#newtext-${postid}`).innerHTML = `
-      <form>
-          <textarea class="form-control" id="newpost-${postid}" >${oldpost}</textarea>
-          <input type="submit" value="Change" class="btn btn-success" style="width:45%;" onclick="changepost( ${page}, ${postid},${oldpost});">
-          <input type="cancel" value="Cancel" class="btn btn-danger"  style="width:45%;" onclick="window.location.reload(true);">
-      </form> `
-
-}
-
-
-function changepost( page, postid, newpost){
+function changepost(postid, page){
+  console.log('Inside changepost');
   const url = `/${page}/${postid}`;
-  newtext = document.querySelector(`#newpost-${id}`).value;
+  newtext = document.querySelector(`#newpost-${spostid}`).value;
   fetch(url, {
     method: "PUT",
     body: JSON.stringify({
        postid:`${postid}`,
        page: `${page}`,
-       newtext:`${newtext}`,
+       newtext:`${newpost}`,
     })
   })
   .then(response => response.json())
