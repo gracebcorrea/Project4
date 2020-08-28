e5177119909ad59748f5854fbd102000c1875f31
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-08-27 23:26:17 -0300

    Update editposttest.js

--- network/static/network/editposttest.js
+++ network/static/network/editposttest.js
@@ -45,6 +45,42 @@ function openedit(postid){
 
   alert(`${oldtext}`);
 }
+TENTATIVA 2
+function openedit(postid, oldtext) {
+  var x = document.getElementById(`#oldtext-${postid}`);
+  var y = document.getElementById(`#newtext-${postid}`);
+
+  alert(x);
+  alert(y);
+
+
+  if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+  }
+  else {
+      x.style.display = "none";
+      y.style.display = "block";
+  }
+
+  const oldpost = document.getElementById(`oldtext-${postid}`);
+  oldpost.innerHTML= `<p> ${oldtext} </p> `;
+  x.append(oldpost);
+
+  const newpost = document.createElement('form');
+  newpost.innerHTML= `
+      <textarea class="form-control" id="newpost-${postid}" >${oldtext}</textarea>
+      <input type="submit" value="Change" class="btn btn-success" style="width:45%;" onclick="changepost( ${postid}, 'profile');">
+      <input type="cancel" value="Cancel" class="btn btn-danger"  style="width:45%;" onclick="window.location.reload(true);">
+      `;
+  y.append(newpost);
+
+}
+
+
+
+
+
 
 function changepost(postid, page){
   console.log('Inside changepost');
