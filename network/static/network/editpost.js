39854d415fc4cfb13428967d49acd9ad01e12350
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-08-27 21:45:19 -0300

    Update editpost.js

--- network/static/network/editpost.js
+++ network/static/network/editpost.js
@@ -1,12 +1,25 @@
-function openedit(postid) {
+function openedit(postid, oldtext) {
   var x = document.getElementById(`oldtext-${postid}`);
   var y = document.getElementById(`newtext-${postid}`);
-  if (x.style.display === "none") {
-    x.style.display = "block";
-    y.style.display = "none";
-  } else {
+
+  const oldpost = document.getElementById(`oldtext-${postid}`);
+  oldpost.innerHTML= `<p> ${oldtext} </p> `;
+  x.append(oldpost);
+
+  const newpost = document.createElement('form');
+  newpost.innerHTML= `
+      <textarea class="form-control" id="newpost-${postid}" >${oldtext}</textarea>
+      <input type="submit" value="Change" class="btn btn-success" style="width:45%;" onclick="changepost( ${postid}, 'profile');">
+      <input type="cancel" value="Cancel" class="btn btn-danger"  style="width:45%;" onclick="window.location.reload(true);">
+      `;
+  y.append(newpost);
+
+  if (x.style.display === "block") {
     x.style.display = "none";
     y.style.display = "block";
+  } else {
+    x.style.display = "block";
+    y.style.display = "none";
   }
 }
 
